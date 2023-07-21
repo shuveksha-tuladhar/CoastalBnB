3e812d41db8cb4e846e7d09b707eac09874433f1
Shuveksha <tuladhar.shuveksha@gmail.com>
2023-07-21 13:00:04 -0700

    Modified social-icon, added logo

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -5,17 +5,10 @@ function Header() {
     return (
         <div id='header-bar'>
         <Link id='logo' to='/'>
-            {/* <img id='logo-img' alt="Logo here..." src={logo} /> */}
-            Logo Here...
-            <span id='span-logo'>Coast BnB</span>
+            <img id='logo-img' alt="Logo here..." src='assets/logo-white.jpg' />
+            
         </Link>
-        <a href="https://github.com/shuveksha-tuladhar" target="_blank" rel="noreferrer">
-            <img className="icons-header" alt="Github" width="32" height="32" src="../../assets/github.png"/>
-        </a>
-        <br/>
-        <a href="https://www.linkedin.com/in/shuvekshatuladhar/" target="_blank" rel="noreferrer">
-            <img className="icons-header" alt="LinkedIn" width="32" height="32" src="../../assets/linkedin.png"/>
-        </a>
+        
         </div>
         )
     }
