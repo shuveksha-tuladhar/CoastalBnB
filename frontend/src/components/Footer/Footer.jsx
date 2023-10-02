949d3d6f25d8c6ea9f2192999ba321c68719df67
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2023-10-02 16:26:53 -0700

    Added footer and social icons

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import {BiSolidUserRectangle} from 'react-icons/bi';
+import "./Footer.css";
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className='footer'>
+      <div className='footer_section'>
+        <div className='footer_subsection'>
+          <span>© {currentYear} - CoastalBnB by Shuveksha Tuladhar</span>
+          <a href="https://github.com/shuveksha-tuladhar" target="_blank" rel="noreferrer">
+          <FaGithubSquare size={28} className="icons-footer"  />
+        
+            </a>
+            <br/>
+            <a href="https://www.linkedin.com/in/shuvekshatuladhar/" target="_blank" rel="noreferrer">
+            <FaLinkedin size={28} className="icons-footer"/>
+          
+            </a>
+            <a href="https://shuveksha-tuladhar.github.io/" target="_blank" rel="noreferrer">
+            <BiSolidUserRectangle size={28} className="icons-footer"/>
+                {/* <img className="icons-footer" alt="Website" width="24" height="24" src="../../assets/profile.png"/> */}
+            </a>
+        </div>
+       </div>
+    </footer>
+  );
+}
